06823b195ee97f392b24042aa0a19b67b091950c
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-21 15:07:13 -0300

    chore: buildong profile component

---
+++ src/profile/entities/profile.entity.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Game } from 'src/games/entities/game.entity';
+import { User } from 'src/User/entities/user.entities';
+
+export class Profile {
+  id?: string;
+  title: string;
+  imageUrl: string;
+  user?: User;
+  games?: Game[];
+  createdAt?: Date;
+  updatedAt?: Date;
+}
