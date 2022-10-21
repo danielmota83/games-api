fb4ece592cd9e2e58431948232d833aa49cbf6b6
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-21 15:01:37 -0300

    chore: bulding user entitie component

---
+++ src/User/entities/user.entities.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+export class User {
+    id?: string;
+    name: string;
+    nickname: string;
+    email: string;
+    password: string;
+    cpf: string;
+    isAdmin: boolean;
+    createdAt?: Date;
+    updatedAt?: Date;
+  }
---
+++ src/User/user.controller.ts
@@ -0,0 +1,77 @@
+/* eslint-disable prettier/prettier */
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Patch,
+    Post,
+    UseGuards,
+  } from '@nestjs/common';
+  import { AuthGuard } from '@nestjs/passport';
+  import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+  import { LoggedUser } from 'src/auth/logged-user.decorator';
+
+  import { CreateUserDto } from './dto/create-user.dto';
+  import { UpdateUserDto } from './dto/update-user.dto';
+  import { User } from './entities/user.entities';
+  import { UserService } from './user.service';
+  
+  @ApiTags('users')
+  @Controller('user')
+  export class UserController {
+    constructor(private readonly userService: UserService) {}
+  
+    @Get()
+    @UseGuards(AuthGuard())
+    @ApiBearerAuth('JWT')
+    @ApiOperation({
+      summary: 'Listar usúarios',
+    })
+    findAll(@LoggedUser() user: User): Promise<User[]> {
+      return this.userService.findAll(user);
+    }
+  
+    @Get(':id')
+    @UseGuards(AuthGuard())
+    @ApiBearerAuth('JWT')
+    @ApiOperation({
+      summary: 'Visualizar usúario',
+    })
+    findOne(@Param('id') id: string): Promise<User> {
+      return this.userService.findOne(id);
+    }
+  
+    @Post()
+    @ApiOperation({
+      summary: 'Criar cadastro de usúario',
+    })
+    create(@Body() dto: CreateUserDto): Promise<User> {
+      return this.userService.create(dto);
+    }
+  
+    @Patch(':id')
+    @UseGuards(AuthGuard())
+    @ApiBearerAuth('JWT')
+    @ApiOperation({
+      summary: 'Editar informações do usúario',
+    })
+    update(@Param('id') id: string, @Body() dto: UpdateUserDto): Promise<User> {
+      return this.userService.update(id, dto);
+    }
+  
+    @Delete(':id')
+    @UseGuards(AuthGuard())
+    @ApiBearerAuth('JWT')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @ApiOperation({
+      summary: 'Deletar Usúario',
+    })
+    delete(@Param('id') id: string) {
+      this.userService.delete(id);
+    }
+  }
+  
