1f2efe5f283139e83508861d7921ea304175b2cb
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-22 10:45:16 -0300

    chore: building auth component

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PrismaModule,
+    PassportModule.register({defaultStrategy: 'jwt'}),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+})
+export class AuthModule {}
